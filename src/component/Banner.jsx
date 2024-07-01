8572ee8e805e47c2d6f4044eb29e31ae884ec7b2
muhammad haseen <hasinmmpm786@gmail.com>
2024-07-01 15:35:03 +0530

    added banner

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -2,14 +2,14 @@ import banner from "/banner.png";
 
 const Banner = () => {
   return (
-    <div className="sitesize bg-slate-50 ">
+    <div className="sitesize bg-slate-5npm 0 ">
       <div className=" flex flex-col-reverse mt-10  md:flex-row justify-between items-center gap-8 ">
         {/* caption */}
         <div className="md:w-1/2 py-8 md:py-24 space-y-7">
           <h2 className="text-4xl md:text-6xl font-bold font-heading tracking-wide  text-yellow ">
             Delve into the <span className="text-red">Joy</span>
             <br />
-            of delicious dining{" "}
+            of delicious dining
           </h2>
           <p className="text-secondary mt-10 font-semibold text-lg pr-10">
             From comforting classics to bold, new flavors, we bring you an
