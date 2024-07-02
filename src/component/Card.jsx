d49fc3305ba241b629711470b158baecd85b8211
muhammad haseen <hasinmmpm786@gmail.com>
2024-07-02 07:11:30 +0530

    card added

---
+++ src/component/Card.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Card = ({ cardItem }) => {
+  return (
+    <div className=" py-14">
+      <Link to={`/menu/${cardItem._id}`}>
+        <div className=" transition ease-in-out delay-150 cursor-pointer hover:bg-slate-50 rounded-xl hover:-translate-y-1 hover:scale-110  duration-300  card bg-white text-black w-96 hover:shadow-xl">
+          <figure>
+            <img src={cardItem.image} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{cardItem.name}</h2>
+            <p>{cardItem.recipe}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
